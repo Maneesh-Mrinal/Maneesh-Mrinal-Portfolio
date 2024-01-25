c88af27d23825501dbc348025fe1f35b78547ea7
Maneesh-Mrinal <maneesh.mrinal6@gmail.com>
2024-01-25 21:53:46 +0530

    Added photography section

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  const photos = document.querySelectorAll('.photo');
+
+  photos.forEach(photo => {
+    photo.addEventListener('click', () => {
+      const imageUrl = photo.getAttribute('src');
+      window.open(imageUrl, '_blank');
+    });
+  });
+
